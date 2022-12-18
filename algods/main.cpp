6c0bf82808f0b8fc0c7e36c0999f1ddc6421ea90
abanob.anis <abanob.anis@fateel.co>
2022-12-18 22:16:49 +0200

    git_connect

---
+++ algods/main.cpp
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/cppFiles/main.cc to edit this template
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: mac
+ *
+ * Created on December 18, 2022, 10:14 PM
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    
+    cout << "Hello!" << endl;
+
+    return 0;
+}
+
