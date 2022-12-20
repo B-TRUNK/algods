9c4f46b047eec62f4d2bf92e16f395f7c32ddbea
abanob.anis <abanob.anis@fateel.co>
2022-12-20 13:44:57 +0200

    arr_search

--- algods/main.cpp
+++ algods/main.cpp
@@ -12,16 +12,36 @@
 
 #include <cstdlib>
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 /*
  * 
  */
-int main(int argc, char** argv) {
+void main() {
     
-    cout << "Hello!" << endl;
-
-    return 0;
+    //array search
+    int input = 0;
+    int index = 0;
+    int inc = 0;
+    char arr[] = {22 ,30 ,7 ,-3 ,97 ,101 ,0};
+    
+    cout << "Enter a number to Search :" << endl;
+    cin >> input;
+    
+    for ( inc = 0 ;inc < 7 ;inc++ ) {
+    
+        if (arr[inc] == input) {
+            index = inc;
+            
+            break;
+        }
+    
+    }
+    
+    
+    
+    
 }
 
